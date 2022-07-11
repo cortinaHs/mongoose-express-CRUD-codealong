e14653e1fd7415786fa357097e4495a205b1d289
Lisa Hilterhaus <lisa@hilterhaus.net>
2022-07-11 14:58:07 +0200

    add book details page and filter by name and rating

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -5,19 +5,44 @@ const router = require("express").Router();
 /* GET home page */
 router.get("/books", (req, res, next) => {
 
-  Book.find()
+  let filter = {};
+
+  if(req.query.title) {
+    filter.title = {"$regex": req.query.title, "$options": "i"}
+  }
+
+  if(req.query.rating){
+    filter.rating = {$gte: req.query.rating}
+  }
+
+  console.log(filter)
+
+  Book.find(filter)
     .then( (booksFromDB) => {
         console.log(booksFromDB)
         res.render("books/books-list", { books: booksFromDB })
     })
-    .catch( (err) => console.log("Error getting data from DB:", err))
+    .catch( (err) => {
+      console.log("Error getting data from DB:", err)
+      next(error)
+    })
 });
 
 router.get('/books/:bookId', (req, res) => {
 
+  const { bookId } = req.params;
 
-  
-  res.render('books/book-details.hbs')
+  Book.findById(bookId)
+    .then(book => {
+      console.log(book)
+      res.render('books/book-details.hbs', {book})
+    })
+    .catch(error => {
+      console.log('Error while retrieving book details: ', error);
+ 
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    })
 });
 
 module.exports = router;
